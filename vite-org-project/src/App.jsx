887977a4ad417e608719a8315a93f013d3167d09
lijian <ljhhh@qq.com>
2022-07-21 22:37:44 +0800

    vite脚手架-添加依赖

---
+++ vite-org-project/src/App.jsx
@@ -0,0 +1,9 @@
+import {defineComponent} from "vue";
+import  './style.css'
+export default defineComponent({
+    setup(){
+        return ()=>{
+            return <div class="customer">Hello vite-vueJsx</div>
+        }
+    }
+})
