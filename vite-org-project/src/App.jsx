afe7e9b3c27c0c040587d65a5d9a71f49146f253
lijian <ljhhh@qq.com>
2022-07-21 23:05:52 +0800

    vite脚手架-css 添加stylus,css预处理器

--- vite-org-project/src/App.jsx
+++ vite-org-project/src/App.jsx
@@ -1,10 +1,11 @@
 import {defineComponent} from "vue";
 import  '@styles/index.css'
 import classes from '@styles/test.module.css'
+import '@styles/test.styl'
 export default defineComponent({
     setup(){
         return ()=>{
-            return <div class={ `root ${classes.moduleClass}`}>Hello vite-vueJsx</div>
+            return <div class={ `root ${classes.moduleClass} big`}>Hello vite-vueJsx</div>
         }
     }
 })
