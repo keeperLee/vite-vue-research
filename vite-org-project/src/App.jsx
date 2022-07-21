04a14db35927505243dbf9c4b38f58050ce02521
lijian <ljhhh@qq.com>
2022-07-21 22:57:52 +0800

    vite脚手架-css module使用

--- vite-org-project/src/App.jsx
+++ vite-org-project/src/App.jsx
@@ -1,9 +1,10 @@
 import {defineComponent} from "vue";
 import  '@styles/index.css'
+import classes from '@styles/test.module.css'
 export default defineComponent({
     setup(){
         return ()=>{
-            return <div class="customer">Hello vite-vueJsx</div>
+            return <div class={ `root ${classes.moduleClass}`}>Hello vite-vueJsx</div>
         }
     }
 })
