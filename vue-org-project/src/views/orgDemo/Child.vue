9d024e497ef46a639811e3aa6faf9fa7c25b1023
lijian <1376623234@qq.com>
2022-07-25 16:08:41 +0800

    vue3官网的脚手架-setup方式，子组件defineExpose数据给父组件

--- vue-org-project/src/views/orgDemo/Child.vue
+++ vue-org-project/src/views/orgDemo/Child.vue
@@ -3,14 +3,17 @@
 </template>
 
 <script setup>
-// import {ref} from 'vue'
-// const a = 1
-// const b = ref(2)
-//
-// defineExpose({
-//   a,
-//   b
-// })
+//有一个例外的情况，使用了 <script setup> 的组件是默认私有的：
+// 一个父组件无法访问到一个使用了 <script setup> 的子组件中的任何东西，
+// 除非子组件在其中通过 defineExpose 宏显式暴露：
+import {ref} from 'vue'
+const a = 1
+const b = ref(2)
+
+defineExpose({
+  a,
+  b
+})
 </script>
 
 <style scoped>
