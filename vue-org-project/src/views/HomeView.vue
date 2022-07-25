523c265ee143da2ad20e338f1100011d7522aa6a
lijian <1376623234@qq.com>
2022-07-25 09:22:45 +0800

    vue3官网的脚手架-reactive-ref-computed

--- vue-org-project/src/views/HomeView.vue
+++ vue-org-project/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '@/components/TheWelcome.vue'
+import DemoOne from '@/views/orgDemo/DemoOne.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <DemoOne/>
+<!--    <TheWelcome />-->
   </main>
 </template>
---
+++ vue-org-project/src/views/orgDemo/DemoOne.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>Has published books:</p>
+    <span>{{ publishedBooksMessage }}</span>
+  </div>
+</template>
+
+<script setup>
+
+import {reactive, ref, computed} from "vue";
+
+// const raw = {}
+// const proxy = reactive(raw)
+// console.log(proxy)
+// console.log(reactive(raw) === proxy)
+// console.log(reactive(proxy) === proxy)
+
+// const count = ref(0)
+// console.log(count)
+// console.log(count.value)
+
+
+const author = reactive({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Advanced Guide',
+    'Vue 3 - Basic Guide',
+    'Vue 4 - The Mystery'
+  ]
+})
+
+// 一个计算属性 ref
+const publishedBooksMessage = computed(() => {
+  return author.books.length > 0 ? 'Yes' : 'No'
+})
+
+
+</script>
+
+<style scoped>
+
+</style>
