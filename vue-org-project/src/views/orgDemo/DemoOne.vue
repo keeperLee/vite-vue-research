aefaa6736ae2683509c25b7ff5d88aef4e0de102
lijian <1376623234@qq.com>
2022-07-25 10:18:50 +0800

    vue3官网的脚手架-DOM原生事件

--- vue-org-project/src/views/orgDemo/DemoOne.vue
+++ vue-org-project/src/views/orgDemo/DemoOne.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <p>Has published books:</p>
-    <span>{{ publishedBooksMessage }}</span>
-    <br>
-    <span>{{fullName}}</span>
-    <button @click="changeName">修改名字</button>
+    <!-- 使用特殊的 $event 变量 -->
+    <button @click="warn('Form cannot be submitted yet.', $event)">
+      Submit
+    </button>
+
+    <!-- 使用内联箭头函数 -->
+    <button @click="(event) => warn('Form cannot be submitted yet.', event)">
+      Submit
+    </button>
   </div>
 </template>
 
@@ -23,38 +27,48 @@ import {reactive, ref, computed} from "vue";
 // console.log(count.value)
 
 
-const author = reactive({
-  name: 'John Doe',
-  books: [
-    'Vue 2 - Advanced Guide',
-    'Vue 3 - Basic Guide',
-    'Vue 4 - The Mystery'
-  ]
-})
+// const author = reactive({
+//   name: 'John Doe',
+//   books: [
+//     'Vue 2 - Advanced Guide',
+//     'Vue 3 - Basic Guide',
+//     'Vue 4 - The Mystery'
+//   ]
+// })
+//
+// // 一个计算属性 ref
+// const publishedBooksMessage = computed(() => {
+//   return author.books.length > 0 ? 'Yes' : 'No'
+// })
+//
+//
+// const firstName = ref('John')
+// const lastName = ref('Doe')
+//
+// const fullName = computed({
+//   // getter
+//   get() {
+//     return firstName.value + ' ' + lastName.value
+//   },
+//   // setter
+//   set(newValue) {
+//     // 注意：我们这里使用的是解构赋值语法
+//     [firstName.value, lastName.value] = newValue.split(' ')
+//   }
+// })
+//
+// const changeName = () => {
+//   fullName.value = 'li jian'
+// }
 
-// 一个计算属性 ref
-const publishedBooksMessage = computed(() => {
-  return author.books.length > 0 ? 'Yes' : 'No'
-})
 
-
-const firstName = ref('John')
-const lastName = ref('Doe')
-
-const fullName = computed({
-  // getter
-  get() {
-    return firstName.value + ' ' + lastName.value
-  },
-  // setter
-  set(newValue) {
-    // 注意：我们这里使用的是解构赋值语法
-    [firstName.value, lastName.value] = newValue.split(' ')
+function warn(message, event) {
+  console.log(event)
+  // 这里可以访问原生事件
+  if (event) {
+    event.preventDefault()
   }
-})
-
-const changeName = () => {
-  fullName.value = 'li jian'
+  alert(message)
 }
 </script>
 
