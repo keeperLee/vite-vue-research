9ec86007cd84ff46253433c8006502b40b76c9e6
lijian <ljhhh@qq.com>
2022-07-26 22:26:32 +0800

    typescript-基本数据类型

---
+++ typescript-research/basic-types.ts
@@ -0,0 +1,23 @@
+//boolean类型
+let isDone: boolean = false
+
+//number类型
+let age: number = 10
+
+//string类型
+let firstName: string = 'lijian'
+let message: string = `hello ${firstName}`
+
+//undefined和null是所有类型的子类型
+let u: undefined = undefined
+let n: null = null
+let num: number = undefined
+
+//any类型
+let notSure: any = 4
+notSure = 'a str string'
+notSure = true
+notSure.myName
+notSure.getName()
+
+
