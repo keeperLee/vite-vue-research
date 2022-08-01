7b6f8df8de8c8bbd3c1295da70c3e30e46953089
lijian <1376623234@qq.com>
2022-08-01 09:03:50 +0800

    vue3官网的脚手架-nextTick更新

--- vue-org-project/src/views/HomeView.vue
+++ vue-org-project/src/views/HomeView.vue
@@ -2,11 +2,13 @@
 import TheWelcome from '@/components/TheWelcome.vue'
 import DemoOne from '@/views/orgDemo/DemoOne.vue'
 import DemoTwo from '@/views/orgDemo/DemoTwo.vue'
+import DemoThree from '@/views/orgDemo/DemoThree.vue'
 </script>
 
 <template>
   <main>
-    <DemoTwo/>
+<!--    <DemoTwo/>-->
+    <DemoThree/>
 <!--    <TheWelcome />-->
   </main>
 </template>
