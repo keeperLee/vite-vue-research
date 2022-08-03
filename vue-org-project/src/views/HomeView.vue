c4ada1be55a0d5480fdab46efae423b5744da64b
lijian <1376623234@qq.com>
2022-08-03 10:06:43 +0800

    vue3官网的脚手架-toRefs

--- vue-org-project/src/views/HomeView.vue
+++ vue-org-project/src/views/HomeView.vue
@@ -3,12 +3,13 @@ import TheWelcome from '@/components/TheWelcome.vue'
 import DemoOne from '@/views/orgDemo/DemoOne.vue'
 import DemoTwo from '@/views/orgDemo/DemoTwo.vue'
 import DemoThree from '@/views/orgDemo/DemoThree.vue'
+import DemoFour from '@/views/orgDemo/DemoFour.vue'
 </script>
 
 <template>
   <main>
 <!--    <DemoTwo/>-->
-    <DemoThree/>
+    <DemoFour/>
 <!--    <TheWelcome />-->
   </main>
 </template>
---
+++ vue-org-project/src/views/orgDemo/DemoFour.vue
@@ -0,0 +1,29 @@
+<script>
+import { reactive,toRefs } from 'vue'
+export default {
+  setup(){
+    let data = reactive({
+      first:'124',
+      second:'233',
+      third:'666'
+    })
+    let obj = toRefs(data)
+    return {
+      ...obj
+    }
+  }
+}
+
+
+
+</script>
+
+<template>
+  <div class="demo">
+    <div>{{first}}</div>
+    <div>{{second}}</div>
+    <div>{{third}}</div>
+    <button @click="first++">增加</button>
+
+  </div>
+</template>
